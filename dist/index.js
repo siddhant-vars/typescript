54891e08a91586ab1e14168f1256cd9f9eeb767a
siddhant-vars <siddhantvarshney446@gmail.com>
2026-02-01 17:33:47 +0530

    feat: initialize TypeScript project with basic setup
    - Added TypeScript package (version 5.9.3) with package.json and package-lock.json
    - Created initial project structure with src/index.ts containing a simple greet function
    - Configured TypeScript compiler options in tsconfig.json for project settings
    - Defined npm scripts for development and starting the application

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `Hello ${name}, Welocme`;
+}
+const username = "siddhant";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function greet(name:string) : string {
+    return `Hello ${name}, Welocme`
+}
+
+const username:string = "siddhant"
+console.log(greet(username));
